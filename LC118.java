1548bd7d435857a43a50a4f1e5db45e7d7f659e3
Dingqinliu <dingqinliu@163.com>
2021-05-28 20:32:12 +0800

    给定非负整数打印杨辉三角

---
+++ LC118.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class LC118 {class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for(int i = 0; i < numRows; i++){
+            List<Integer> temp = new ArrayList<>();
+            if(i == 0){
+                temp.add(1);
+            }else{
+                List<Integer> list = ans.get(i - 1);
+                int size = list.size() + 1;
+                for(int j = 0; j < size; j++){
+                    if(j == 0 || j == size - 1){
+                        temp.add(1);
+                    }else{
+                        temp.add(list.get(j - 1) + list.get(j));
+                    }
+                }
+            }
+            ans.add(temp);
+        }
+        return ans;
+    }
+}
+}

1d959cce3fafc1cf87316e5d0bf63318975c3a4d
Dingqinliu <dingqinliu@163.com>
2021-06-02 22:21:52 +0800

    从尾到头打印链表

--- JZ06.java
+++ JZ06.java
@@ -6,20 +6,34 @@ public class JZ06 {
 //        思路2 用栈 利用先进后出
 
 
-        ListNode cur=head; //思路1
-        int count=0;
-        while(cur!=null){
-            count++;
+//        ListNode cur=head; //思路1
+//        int count=0;
+//        while(cur!=null){
+//            count++;
+//            cur=cur.next;
+//        }
+//        int[] array=new int[count];
+//        cur=head;
+//            for (int j=count-1;j>=0;j--){
+//                array[j]=cur.val;
+//                cur=cur.next;
+//            }
+//        return array;
+
+        Stack<ListNode> stack=new Stack<ListNode>();
+        ListNode cur=head;
+        while (cur!=null){
+            stack.push(cur);
             cur=cur.next;
         }
-        int[] array=new int[count];
-        cur=head;
-            for (int j=count-1;j>=0;j--){
-                array[j]=cur.val;
-                cur=cur.next;
-            }
+
+        int size=stack.size();
+        int[] array=new int[size];
+        for (int j=0;j<size;j++){
+            array[j]=stack.peek().val;
+            stack.pop();
+        }
+
         return array;
-
-
     }
 }

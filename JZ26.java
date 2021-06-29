b35a2efc7b776ba69aadf655b03253d959c6c507
Dingqinliu <dingqinliu@163.com>
2021-06-29 23:12:21 +0800

    判断树的子结构

---
+++ JZ26.java
@@ -0,0 +1,16 @@
+package Package;
+
+//输入两棵二叉树A和B，判断B是不是A的子结构。(约定空树不是任意一个树的子结构)
+//B是A的子结构， 即 A中有出现和B相同的结构和节点值。
+//
+public class JZ26 {
+    public boolean isSubStructure(TreeNode A, TreeNode B){
+        if (A==null||B==null) return false;
+        return dfs(A,B)||isSubStructure(A.left,B)||isSubStructure(A.right,B);
+    }
+    public boolean dfs(TreeNode A, TreeNode B){
+        if (B==null) return true;
+        if (A==null) return false;
+        return A.val==B.val && dfs(A.left, B.left) && dfs(A.right, B.right);
+    }
+}

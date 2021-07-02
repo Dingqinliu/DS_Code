9fe9cba542591b4d7165b256024b61c6bc909adc
Dingqinliu <dingqinliu@163.com>
2021-07-02 22:45:10 +0800

    两个栈实现一个队列

--- JZ09.java
+++ JZ09.java
@@ -1,35 +1,52 @@
-//用两个栈实现队列
-//分别完成在队列尾部插入整数 和在队列头部删除整数的功能
+package review_2;
+
+import com.sun.xml.internal.ws.policy.EffectiveAlternativeSelector;
 
 import java.util.LinkedList;
-import java.util.Stack;
+import java.util.concurrent.LinkedBlockingQueue;
 
-class CQueue {
-    LinkedList<Integer> st1;
-    LinkedList<Integer> st2;
-
+//两个栈实现一个队列
+//栈 Stack 先进后出 水杯结构
+//队列 Queue 先进先出 排队结构
+//用LinkedList作为栈的容器是因为：
+// --------------------
+// LinkedList 是一个继承于AbstractSequentialList的双向链表。
+//LinkedList 可以被当作堆栈、队列或双端队列进行操作。
+//LinkedList 实现 List 接口，所以能对它进行队列操作。
+//LinkedList 实现 Deque 接口，能将LinkedList当作双端队列使用。
+//LinkedList 实现了Cloneable接口，即覆盖了函数clone()，能克隆。
+//LinkedList 实现java.io.Serializable接口，所以LinkedList支持序列化，能通过序列化去传输。
+//LinkedList 是非同步的。
+//————————————————
+//如果你使用Stack的方式来做这道题，会造成速度较慢；
+// 原因的话是Stack继承了Vector接口，而Vector底层是一个Object[]数组，那么就要考虑空间扩容和移位的问题了。
+// 可以使用LinkedList来做Stack的容器，因为LinkedList实现了Deque接口，所以Stack能做的事LinkedList都能做，
+// 其本身结构是个双向链表，扩容消耗少。 但是我的意思不是像100%代码那样直接使用一个LinkedList当做队列，那确实是快，但是不符题意。
+class CQueue{
+    LinkedList<Integer> stack1;
+    LinkedList<Integer> stack2;
     public CQueue(){
-        st1=new LinkedList<>();
-        st2=new LinkedList<>();
+        stack1=new LinkedList<>();
+        stack2=new LinkedList<>();
     }
 
     public void appendTail(int value){
-        st1.add(value);
+        stack1.add(value);
     }
 
     public int deleteHead(){
-        if (st2.isEmpty()){
-            if (st1.isEmpty()){
+        if (stack1.isEmpty()){
+            if (stack2.isEmpty()){
                 return -1;
             }
-            while (!st1.isEmpty()){
-                st2.add(st1.pop());
+            while (!stack2.isEmpty()){
+                stack1.add(stack2.pop());
             }
-            return st2.pop();
+            return stack1.pop();
+        }else {
+            stack1.add(stack2.pop());
+            return stack1.pop();
         }
-        else {
-            return st2.pop();
-        }
-    }
 
+    }
 }

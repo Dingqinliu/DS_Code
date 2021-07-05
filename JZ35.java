45c2bdbfa124bdbeead4dabcd2c4700fea8f4e36
Dingqinliu <dingqinliu@163.com>
2021-07-05 22:03:05 +0800

    复杂链表的复制

---
+++ JZ35.java
@@ -0,0 +1,41 @@
+package Package;
+
+public class JZ35 {
+    public Node copyRandomList(Node head) {
+        if (head == null) {
+            return head;
+        }
+        // 完成链表节点的复制
+        Node cur = head;
+        while (cur != null) {
+            Node copyNode = new Node(cur.val);
+            copyNode.next = cur.next;
+            cur.next = copyNode;
+            cur = cur.next.next;
+        }
+
+        // 完成链表复制节点的随机指针复制
+        cur = head;
+        while (cur != null) {
+            if (cur.random != null) { // 注意判断原来的节点有没有random指针
+                cur.next.random = cur.random.next;
+            }
+            cur = cur.next.next;
+        }
+
+        // 将链表一分为二
+        Node copyHead = head.next;
+        cur = head;
+        Node curCopy = head.next;
+        while (cur != null) {
+            cur.next = cur.next.next;
+            cur = cur.next;
+            if (curCopy.next != null) {
+                curCopy.next = curCopy.next.next;
+                curCopy = curCopy.next;
+            }
+        }
+        return copyHead;
+    }
+}
+}
